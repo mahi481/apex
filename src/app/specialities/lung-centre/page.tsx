efe0e887f40c05c3acc9c646dd992a025b2e4080
mahi481 <iammahima71@gmail.com>
2025-08-01 19:29:00 +0530

    Update page.tsx

--- src/app/specialities/lung-centre/page.tsx
+++ src/app/specialities/lung-centre/page.tsx
@@ -68,7 +68,7 @@ export default function LungCentrePage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="/images/kidney.jpg"
                 alt="Lung Centre Department"
                 width={600}
                 height={400}
