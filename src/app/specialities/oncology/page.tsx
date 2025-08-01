d0e83a43c89c5373714600d4575c49e856fe175d
mahi481 <iammahima71@gmail.com>
2025-08-01 19:35:27 +0530

    Update page.tsx

--- src/app/specialities/oncology/page.tsx
+++ src/app/specialities/oncology/page.tsx
@@ -68,7 +68,7 @@ export default function OncologyPage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="/images/onco.jpg"
                 alt="Oncology Department"
                 width={600}
                 height={400}
