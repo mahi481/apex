b4dbcca12a09b0dcc5f959c88d60f63eaaeac05a
mahi481 <iammahima71@gmail.com>
2025-08-01 19:37:49 +0530

    Update page.tsx

--- src/app/specialities/plastic-surgery/page.tsx
+++ src/app/specialities/plastic-surgery/page.tsx
@@ -68,7 +68,7 @@ export default function PlasticSurgeryPage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="/images/plasticsurgery.jpg"
                 alt="Plastic Surgery Department"
                 width={600}
                 height={400}
