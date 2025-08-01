10e951290699e257a2a2ce931f08dd2e0b07a705
mahi481 <iammahima71@gmail.com>
2025-08-01 19:26:31 +0530

    Update page.tsx

--- src/app/specialities/interventional-radiology/page.tsx
+++ src/app/specialities/interventional-radiology/page.tsx
@@ -68,7 +68,7 @@ export default function InterventionalRadiologyPage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="//images/interventional.jpg"
                 alt="Interventional Radiology Department"
                 width={600}
                 height={400}
