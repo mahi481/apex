f6f838e6cf5a0ae4e94a56929c29ce9275964e59
mahi481 <iammahima71@gmail.com>
2025-08-01 19:11:44 +0530

    Update page.tsx

--- src/app/specialities/gastroenterology/page.tsx
+++ src/app/specialities/gastroenterology/page.tsx
@@ -68,7 +68,7 @@ export default function GastroenterologyPage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="/images/gastro.jpg"
                 alt="Gastroenterology Department"
                 width={600}
                 height={400}
