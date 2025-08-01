a88013b3d05db7bb5a7bcb950fe7e96c0e9925d4
mahi481 <iammahima71@gmail.com>
2025-08-01 19:08:28 +0530

    Update page.tsx

--- src/app/specialities/endocrinology/page.tsx
+++ src/app/specialities/endocrinology/page.tsx
@@ -68,7 +68,7 @@ export default function EndocrinologyPage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="/images/endocrinology.jpg"
                 alt="Endocrinology Department"
                 width={600}
                 height={400}
