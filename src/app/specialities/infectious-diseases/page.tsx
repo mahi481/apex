938a6c391a6102f260ea7cf2fed53065e74d0faf
mahi481 <iammahima71@gmail.com>
2025-08-01 19:25:03 +0530

    Update page.tsx

--- src/app/specialities/infectious-diseases/page.tsx
+++ src/app/specialities/infectious-diseases/page.tsx
@@ -68,7 +68,7 @@ export default function InfectiousDiseasesPage() {
             </div>
             <div className={styles.heroImage}>
               <Image
-                src="/placeholder.svg?height=400&width=600"
+                src="/images/infectious.jpg"
                 alt="Infectious Diseases Department"
                 width={600}
                 height={400}
